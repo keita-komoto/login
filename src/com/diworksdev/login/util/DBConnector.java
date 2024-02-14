2e8c2a6d2ed88e8e6240b4c6e0977cd436f3ec6a
keita-komoto <makibiso200x@yahoo.co.jp>
2024-02-14 22:34:24 +0900

    20240214

--- src/com/diworksdev/login/util/DBConnector.java
+++ src/com/diworksdev/login/util/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	private	static String driverName = "com.mysql.cj.jdbc.Driver";
-	private static String url ="jdbc:mysql://localhost/logindb_komoto";
+	private static String url ="jdbc:mysql://localhost/logindb";
 	private	static String user ="root";
 	private static String password ="";
 	
